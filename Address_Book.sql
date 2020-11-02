d41e76408980eb218f1a8e2ad6b91bae197d8526
jaguste.pranav <jaguste.pranav31@gmail.com>
2020-11-02 21:00:01 +0530

    [pranav] Add. Edit entry by searching by name

--- Address_Book.sql
+++ Address_Book.sql
@@ -1,3 +1,23 @@
 create database Address_Book;
 
 use Address_Book;
+
+create table address_book
+(
+first_name varchar(15) not null,
+last_name varchar(15) not null,
+address varchar(50) not null,
+city varchar(10) not null,
+state varchar(10) not null,
+zipcode varchar(10) not null,
+phone_no varchar(10) not null,
+email varchar(15) not null
+);
+
+insert into address_book values
+('Pranav','Jaguste','Mulund','Mumbai','Mh','400081','1234567890','pj@gmail.com'),
+('Someone','Anonymous','Andheri','Mumbai','Mh','400058','9876543210','some@gmail.com')
+
+select* from address_book;
+
+update address_book set address = 'Malad' where first_name = 'Someone';
