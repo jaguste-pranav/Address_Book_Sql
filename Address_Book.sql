c74a1db1152ac52daa2c9a98ee3e82318462fbe1
jaguste.pranav <jaguste.pranav31@gmail.com>
2020-11-02 20:55:10 +0530

    [pranav] Add. Create table called address_book

--- Address_Book.sql
+++ Address_Book.sql
@@ -1,3 +1,15 @@
 create database Address_Book;
 
 use Address_Book;
+
+create table address_book
+(
+first_name varchar(15) not null,
+last_name varchar(15) not null,
+address varchar(50) not null,
+city varchar(10) not null,
+state varchar(10) not null,
+zipcode varchar(10) not null,
+phone_no varchar(10) not null,
+email varchar(15) not null
+);
