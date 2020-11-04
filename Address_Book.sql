53b0c289987252dd8e1c9be5b36890e95842d11c
jaguste.pranav <jaguste.pranav31@gmail.com>
2020-11-04 20:10:12 +0530

    [pranav] Add. Type of contact is added to the address book

--- Address_Book.sql
+++ Address_Book.sql
@@ -14,6 +14,8 @@ phone_no varchar(10) not null,
 email varchar(15) not null
 );
 
+alter table address_book alter column email varchar(25); 
+
 insert into address_book values
 ('Pranav','Jaguste','Mulund','Mumbai','Mh','400081','1234567890','pj@gmail.com'),
 ('Someone','Anonymous','Andheri','Mumbai','Mh','400058','9876543210','some@gmail.com')
@@ -35,3 +37,10 @@ select COUNT(City),city from address_book group by city;
 select COUNT(state),state from address_book group by state;
 
 select * from address_book order by city asc;
+
+alter table address_Book add Contact_Type varchar(20);
+
+update Address_Book set Contact_Type ='Family' where first_name = 'Pranav' or first_name = 'Rahul';
+update Address_Book set Contact_Type ='Friend' where first_name in ('Ram');
+
+
